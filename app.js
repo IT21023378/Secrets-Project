3be18905b1060f1becc39c93d3a9863109c8a334
IT21023378 <it21023378@my.sliit.lk>
2022-01-20 09:27:06 +0530

    Initia Commit, Add lv.2 security and enviroment vars

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const { log } = require("console");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const secret = process.env.SECRET;
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save((err) => {
+    if (!err) {
+      res.render("secrets");
+    } else {
+      res.send(err);
+      console.log(err);
+    }
+  });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (err) {
+      res.send(err);
+    } else if (foundUser) {
+      if (foundUser.password === password) {
+        res.render("secrets");
+      }
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Server started on port 3000`);
+});
